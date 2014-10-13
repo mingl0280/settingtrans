1acc4b0ef7698a40e3e03b1877bc350e898d0837
mingl0280 <mingl0280@gmail.com>
2014-10-13 14:52:10 +0800

    init
    Signed-off-by: mingl0280 <mingl0280@gmail.com>

---
+++ settingtrans/Form1.Designer.vb
@@ -0,0 +1,438 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.NumericUpDown3 = New System.Windows.Forms.NumericUpDown()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.NumericUpDown4 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown5 = New System.Windows.Forms.NumericUpDown()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.NumericUpDown6 = New System.Windows.Forms.NumericUpDown()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox3 = New System.Windows.Forms.RichTextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.RichTextBox4 = New System.Windows.Forms.RichTextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.RichTextBox5 = New System.Windows.Forms.RichTextBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.RichTextBox6 = New System.Windows.Forms.RichTextBox()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown6, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "舰船名称"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(71, 8)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(85, 21)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(316, 12)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 12)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "槽位数量"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(375, 8)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {15, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(34, 21)
+        Me.NumericUpDown1.TabIndex = 3
+        Me.NumericUpDown1.ThousandsSeparator = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(415, 12)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(17, 12)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "高"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(478, 12)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(17, 12)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "中"
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(438, 8)
+        Me.NumericUpDown2.Maximum = New Decimal(New Integer() {15, 0, 0, 0})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(34, 21)
+        Me.NumericUpDown2.TabIndex = 5
+        Me.NumericUpDown2.ThousandsSeparator = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(541, 12)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(17, 12)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "低"
+        '
+        'NumericUpDown3
+        '
+        Me.NumericUpDown3.Location = New System.Drawing.Point(501, 8)
+        Me.NumericUpDown3.Maximum = New Decimal(New Integer() {15, 0, 0, 0})
+        Me.NumericUpDown3.Name = "NumericUpDown3"
+        Me.NumericUpDown3.Size = New System.Drawing.Size(34, 21)
+        Me.NumericUpDown3.TabIndex = 7
+        Me.NumericUpDown3.ThousandsSeparator = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(564, 12)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(53, 12)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "舰船类型"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(623, 8)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox2.TabIndex = 10
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(729, 12)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(29, 12)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "护盾"
+        '
+        'NumericUpDown4
+        '
+        Me.NumericUpDown4.Location = New System.Drawing.Point(764, 8)
+        Me.NumericUpDown4.Maximum = New Decimal(New Integer() {99999, 0, 0, 0})
+        Me.NumericUpDown4.Name = "NumericUpDown4"
+        Me.NumericUpDown4.Size = New System.Drawing.Size(50, 21)
+        Me.NumericUpDown4.TabIndex = 12
+        Me.NumericUpDown4.ThousandsSeparator = True
+        '
+        'NumericUpDown5
+        '
+        Me.NumericUpDown5.Location = New System.Drawing.Point(855, 8)
+        Me.NumericUpDown5.Maximum = New Decimal(New Integer() {99999, 0, 0, 0})
+        Me.NumericUpDown5.Name = "NumericUpDown5"
+        Me.NumericUpDown5.Size = New System.Drawing.Size(50, 21)
+        Me.NumericUpDown5.TabIndex = 14
+        Me.NumericUpDown5.ThousandsSeparator = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(820, 12)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(29, 12)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "装甲"
+        '
+        'NumericUpDown6
+        '
+        Me.NumericUpDown6.Location = New System.Drawing.Point(946, 8)
+        Me.NumericUpDown6.Maximum = New Decimal(New Integer() {99999, 0, 0, 0})
+        Me.NumericUpDown6.Name = "NumericUpDown6"
+        Me.NumericUpDown6.Size = New System.Drawing.Size(50, 21)
+        Me.NumericUpDown6.TabIndex = 16
+        Me.NumericUpDown6.ThousandsSeparator = True
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(911, 12)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(29, 12)
+        Me.Label9.TabIndex = 15
+        Me.Label9.Text = "结构"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 47)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(216, 88)
+        Me.RichTextBox1.TabIndex = 17
+        Me.RichTextBox1.Text = ""
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(12, 32)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(89, 12)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "改装件和无人机"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(232, 32)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(41, 12)
+        Me.Label11.TabIndex = 19
+        Me.Label11.Text = "10人本"
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Location = New System.Drawing.Point(234, 47)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(246, 88)
+        Me.RichTextBox2.TabIndex = 20
+        Me.RichTextBox2.Text = ""
+        '
+        'RichTextBox3
+        '
+        Me.RichTextBox3.Location = New System.Drawing.Point(486, 47)
+        Me.RichTextBox3.Name = "RichTextBox3"
+        Me.RichTextBox3.Size = New System.Drawing.Size(256, 88)
+        Me.RichTextBox3.TabIndex = 22
+        Me.RichTextBox3.Text = ""
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(484, 32)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(41, 12)
+        Me.Label12.TabIndex = 21
+        Me.Label12.Text = "20人本"
+        '
+        'RichTextBox4
+        '
+        Me.RichTextBox4.Location = New System.Drawing.Point(748, 47)
+        Me.RichTextBox4.Name = "RichTextBox4"
+        Me.RichTextBox4.Size = New System.Drawing.Size(246, 88)
+        Me.RichTextBox4.TabIndex = 24
+        Me.RichTextBox4.Text = ""
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(746, 32)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(41, 12)
+        Me.Label13.TabIndex = 23
+        Me.Label13.Text = "40人本"
+        '
+        'RichTextBox5
+        '
+        Me.RichTextBox5.Location = New System.Drawing.Point(14, 153)
+        Me.RichTextBox5.Name = "RichTextBox5"
+        Me.RichTextBox5.Size = New System.Drawing.Size(214, 264)
+        Me.RichTextBox5.TabIndex = 26
+        Me.RichTextBox5.Text = ""
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(10, 138)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(41, 12)
+        Me.Label14.TabIndex = 25
+        Me.Label14.Text = "决战本"
+        '
+        'RichTextBox6
+        '
+        Me.RichTextBox6.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RichTextBox6.Location = New System.Drawing.Point(428, 153)
+        Me.RichTextBox6.Name = "RichTextBox6"
+        Me.RichTextBox6.Size = New System.Drawing.Size(566, 264)
+        Me.RichTextBox6.TabIndex = 28
+        Me.RichTextBox6.Text = ""
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(424, 138)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(77, 12)
+        Me.Label15.TabIndex = 27
+        Me.Label15.Text = "维基文本输出"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(277, 153)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(103, 36)
+        Me.Button1.TabIndex = 29
+        Me.Button1.Text = "转换"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(170, 12)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(53, 12)
+        Me.Label16.TabIndex = 30
+        Me.Label16.Text = "图片名称"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(229, 8)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(81, 21)
+        Me.TextBox3.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(277, 195)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(103, 36)
+        Me.Button2.TabIndex = 31
+        Me.Button2.Text = "清空"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1008, 429)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RichTextBox6)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.RichTextBox5)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.RichTextBox4)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.RichTextBox3)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.NumericUpDown6)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.NumericUpDown5)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.NumericUpDown4)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.NumericUpDown3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "装配转Wiki代码"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown6, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents NumericUpDown2 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents NumericUpDown3 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents NumericUpDown4 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents NumericUpDown5 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents NumericUpDown6 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents RichTextBox2 As System.Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox3 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents RichTextBox4 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents RichTextBox5 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents RichTextBox6 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label15 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label16 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ settingtrans/Form1.vb
@@ -0,0 +1,212 @@
+﻿Public Class Form1
+
+    Dim HighSlot(4), MidSlot(4), LowSlot(4) As String
+    Dim Fixes_Drones As String
+    Const FaD As String = "==改装件和无人机=="
+    Const Fit As String = "<div style=""clear:both"">" + vbCrLf + "==舰船装配==" + vbCrLf + "</div>"
+    Const TableHead As String = "{| class=""article-table"" border=1 style=""width:100% padding:0px; margin:0px;""" + _
+        vbCrLf + "|-" + vbCrLf + _
+        "! scope=""col"" | 槽位" + vbCrLf + _
+        "! scope=""col"" | 10人副本" + vbCrLf + _
+        "! scope=""col"" | 20人副本" + vbCrLf + _
+        "! scope=""col"" | 40人副本" + vbCrLf + _
+        "! scope=""col"" | 决战副本" + vbCrLf + _
+        "|-"
+    Dim tmpl As String = ""
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        processAllString()
+    End Sub
+
+    Private Function processAllString()
+        ReDim HighSlot(4), MidSlot(4), LowSlot(4)
+        Fixes_Drones = ""
+
+        tmpl = "{{舰船属性|舰船名称=" + TextBox1.Text + "|图片名称=" + TextBox3.Text + _
+            "|槽位数量=" + NumericUpDown1.Value.ToString + "高/" + NumericUpDown2.Value.ToString + "中/" + NumericUpDown3.Value.ToString + "低|舰船类型=" + _
+            TextBox2.Text + _
+            "|护盾=" + NumericUpDown4.Value.ToString + "|装甲=" + NumericUpDown5.Value.ToString + "|结构=" + NumericUpDown6.Value.ToString + _
+            "}}" + vbCrLf + vbCrLf
+        RichTextBox6.Text += tmpl
+
+        Dim s As String() = RichTextBox1.Lines
+        Dim currcat As String = ""
+        For i As Integer = 0 To UBound(s)
+            Select Case s(i)
+                Case "改装件插槽"
+                    Fixes_Drones = Fixes_Drones + "===改装件===" + vbCrLf
+                Case "无人机"
+                    Fixes_Drones = Fixes_Drones + "===无人机===" + vbCrLf
+                Case "子系统"
+                    Fixes_Drones = Fixes_Drones + "===子系统===" + vbCrLf
+                Case ""
+                    Continue For
+                Case Else
+                    Fixes_Drones += "* " + s(i) + vbCrLf
+            End Select
+        Next
+        RichTextBox6.Text += "==改装件和无人机==" + vbCrLf
+        RichTextBox6.Text += Fixes_Drones + vbCrLf + vbCrLf
+        RichTextBox6.Text += Fit + vbCrLf + TableHead + vbCrLf
+        ProcessFits()
+    End Function
+
+    Private Function ProcessFits()
+        Dim k1 As String() = RichTextBox2.Lines
+        Dim currcat As String = ""
+        For i As Integer = 0 To UBound(k1)
+            Select Case k1(i)
+                Case "改装件插槽"
+                    Exit For
+                Case "无人机"
+                    Exit For
+                Case "子系统"
+                    Exit For
+                Case "高能量"
+                    HighSlot(0) += "| style=""text-align:center; width:10%;"" | 高槽位" + vbCrLf + "| "
+                    currcat = "h"
+                Case "中级能量"
+                    MidSlot(0) += "|-" + vbCrLf + "| style=""text-align:center; width:10%;"" | 中槽位" + vbCrLf + "| "
+                    currcat = "m"
+                Case "低能量"
+                    LowSlot(0) += "|-" + vbCrLf + "| style=""text-align:center; width:10%;"" | 低槽位" + vbCrLf + "| "
+                    currcat = "l"
+                Case ""
+                    Continue For
+                Case Else
+                    Select Case currcat
+                        Case "h"
+                            HighSlot(0) += k1(i) + "<br />"
+                        Case "m"
+                            MidSlot(0) += k1(i) + "<br />"
+                        Case "l"
+                            LowSlot(0) += k1(i) + "<br />"
+                    End Select
+            End Select
+        Next
+        Dim k2 As String() = RichTextBox3.Lines
+        currcat = ""
+        For i As Integer = 0 To UBound(k2)
+            Select Case k2(i)
+                Case "改装件插槽"
+                    Exit For
+                Case "无人机"
+                    Exit For
+                Case "高能量"
+                    HighSlot(1) += vbCrLf + "| "
+                    currcat = "h"
+                Case "中级能量"
+                    MidSlot(1) += vbCrLf + "| "
+                    currcat = "m"
+                Case "低能量"
+                    LowSlot(1) += vbCrLf + "| "
+                    currcat = "l"
+                Case ""
+                    Continue For
+                Case Else
+                    Select Case currcat
+                        Case "h"
+                            HighSlot(1) += k2(i) + "<br />"
+                        Case "m"
+                            MidSlot(1) += k2(i) + "<br />"
+                        Case "l"
+                            LowSlot(1) += k2(i) + "<br />"
+                    End Select
+            End Select
+        Next
+        Dim k3 As String() = RichTextBox4.Lines
+        currcat = ""
+        For i As Integer = 0 To UBound(k3)
+            Select Case k3(i)
+                Case "改装件插槽"
+                    Exit For
+                Case "无人机"
+                    Exit For
+                Case "高能量"
+                    HighSlot(2) += vbCrLf + "| "
+                    currcat = "h"
+                Case "中级能量"
+                    MidSlot(2) += vbCrLf + "| "
+                    currcat = "m"
+                Case "低能量"
+                    LowSlot(2) += vbCrLf + "| "
+                    currcat = "l"
+                Case ""
+                    Continue For
+                Case Else
+                    Select Case currcat
+                        Case "h"
+                            HighSlot(2) += k3(i) + "<br />"
+                        Case "m"
+                            MidSlot(2) += k3(i) + "<br />"
+                        Case "l"
+                            LowSlot(2) += k3(i) + "<br />"
+                    End Select
+            End Select
+        Next
+        Dim k4 As String() = RichTextBox5.Lines
+        currcat = ""
+        For i As Integer = 0 To UBound(k4)
+            Select Case k4(i)
+                Case "改装件插槽"
+                    Exit For
+                Case "无人机"
+                    Exit For
+                Case "高能量"
+                    HighSlot(3) += vbCrLf + "| "
+                    currcat = "h"
+                Case "中级能量"
+                    MidSlot(3) += vbCrLf + "| "
+                    currcat = "m"
+                Case "低能量"
+                    LowSlot(3) += vbCrLf + "| "
+                    currcat = "l"
+                Case ""
+                    Continue For
+                Case Else
+                    Select Case currcat
+                        Case "h"
+                            HighSlot(3) += k4(i) + "<br />"
+                        Case "m"
+                            MidSlot(3) += k4(i) + "<br />"
+                        Case "l"
+                            LowSlot(3) += k4(i) + "<br />"
+                    End Select
+            End Select
+        Next
+        Dim TableResult(3) As String
+        For i As Integer = 0 To 3
+            TableResult(0) += HighSlot(i).Remove(HighSlot(i).LastIndexOf("<"))
+            TableResult(1) += MidSlot(i).Remove(MidSlot(i).LastIndexOf("<"))
+            TableResult(2) += LowSlot(i).Remove(LowSlot(i).LastIndexOf("<"))
+        Next
+        Dim result = TableResult(0) + vbCrLf + TableResult(1) + vbCrLf + TableResult(2) + vbCrLf + "|}"
+        RichTextBox6.Text += result + vbCrLf + vbCrLf + "{{导航}}"
+    End Function
+
+    Private Sub NumericUpDown1_Enter(sender As Object, e As EventArgs) Handles NumericUpDown6.Enter, NumericUpDown5.Enter, NumericUpDown4.Enter, NumericUpDown3.Enter, NumericUpDown2.Enter, NumericUpDown1.Enter
+        sender.select(0, sender.value.ToString.Length)
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        TextBox3.Text = TextBox1.Text + ".png"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        NumericUpDown1.Value = 0
+        NumericUpDown2.Value = 0
+        NumericUpDown3.Value = 0
+        NumericUpDown4.Value = 0
+        NumericUpDown5.Value = 0
+        NumericUpDown6.Value = 0
+        RichTextBox1.Text = ""
+        RichTextBox2.Text = ""
+        RichTextBox3.Text = ""
+        RichTextBox4.Text = ""
+        RichTextBox5.Text = ""
+        RichTextBox6.Text = ""
+    End Sub
+End Class
